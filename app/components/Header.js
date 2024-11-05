38386c3fce58aff6cf8881445120e0865c2f7652
rasoulie <rasouliee@gmail.com>
2024-11-05 14:50:15 +0330

    header added

---
+++ app/components/Header.js
@@ -0,0 +1,15 @@
+
+import Logo from "./Logo";
+import Navigation from "./Navigation";
+
+export default function Header(){
+
+return (
+    <>
+    <header>
+        <Logo/>
+        <Navigation/>
+    </header>
+    </>
+)
+}
---
+++ app/components/Navigation.js
@@ -0,0 +1,23 @@
+export default function Navigation(){
+   return(
+    <nav>
+     <ul>
+        <li>
+            <a href="#Home">Home</a>
+        </li>
+        <li>
+            <a href="#Projects">Projects</a>
+        </li>
+        <li>
+            <a href="#Services">Services</a>
+        </li>
+        <li>
+            <a href="#Resume">Resume</a>
+        </li>
+        <li>
+            <a href="#Contact">Contact me</a>
+        </li>
+    </ul>
+   </nav>
+   )
+}
